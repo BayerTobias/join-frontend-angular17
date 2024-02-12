8c38206e6ce942c42cab7d80c286d533b27b557c
BayerTobias <tbayer2@gmx.de>
2024-02-12 20:34:28 +0100

    changed folder structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/auth/login/login.component';
+import { SignUpComponent } from './components/auth/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './components/auth/forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './components/auth/reset-password/reset-password.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'reset-password', component: ResetPasswordComponent },
 ];
---
+++ src/app/auth/components/auth-base/auth-base.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-base',
+  standalone: true,
+  imports: [],
+  templateUrl: './auth-base.component.html',
+  styleUrl: './auth-base.component.scss'
+})
+export class AuthBaseComponent {
+
+}
