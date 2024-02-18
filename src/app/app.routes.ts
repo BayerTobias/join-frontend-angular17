b4d5a6a0af4597f6f2e8327aaeaccf9903c2d223
BayerTobias <tbayer2@gmx.de>
2024-02-18 14:50:05 +0100

    created all home components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,11 @@ import { LoginComponent } from './auth/components/login/login.component';
 import { SignUpComponent } from './auth/components/sign-up/sign-up.component';
 import { ForgotPasswordComponent } from './auth/components/forgot-password/forgot-password.component';
 import { ResetPasswordComponent } from './auth/components/reset-password/reset-password.component';
+import { HomeComponent } from './home/components/home/home.component';
+import { SummaryComponent } from './home/components/summary/summary.component';
+import { BoardComponent } from './home/components/board/board.component';
+import { AddTaskComponent } from './home/components/add-task/add-task.component';
+import { ContactsComponent } from './home/components/contacts/contacts.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
@@ -10,4 +15,15 @@ export const routes: Routes = [
   { path: 'sign-up', component: SignUpComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: 'reset-password', component: ResetPasswordComponent },
+  {
+    path: 'home',
+    component: HomeComponent,
+    children: [
+      { path: '', redirectTo: 'summary', pathMatch: 'full' },
+      { path: 'summary', component: SummaryComponent },
+      { path: 'board', component: BoardComponent },
+      { path: 'add-task', component: AddTaskComponent },
+      { path: 'contacts', component: ContactsComponent },
+    ],
+  },
 ];
