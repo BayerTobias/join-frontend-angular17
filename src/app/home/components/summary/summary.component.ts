aa8cfa43fdbe53a6cfe1025668dcbaf41afbc290
BayerTobias <tbayer2@gmx.de>
2024-02-20 19:47:30 +0100

    summary HTML Scss + summary card component

--- src/app/home/components/summary/summary.component.ts
+++ src/app/home/components/summary/summary.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { SummaryCardComponent } from '../../../shared/components/summary-card/summary-card.component';
 
 @Component({
   selector: 'app-summary',
   standalone: true,
-  imports: [],
+  imports: [SummaryCardComponent],
   templateUrl: './summary.component.html',
-  styleUrl: './summary.component.scss'
+  styleUrl: './summary.component.scss',
 })
-export class SummaryComponent {
-
-}
+export class SummaryComponent {}
