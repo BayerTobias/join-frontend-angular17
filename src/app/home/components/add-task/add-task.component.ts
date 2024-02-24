e08a1deba0c3b87c611c264c5babc1b0987eb799
BayerTobias <tbayer2@gmx.de>
2024-02-24 16:37:00 +0100

    add task form group

--- src/app/home/components/add-task/add-task.component.ts
+++ src/app/home/components/add-task/add-task.component.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ButtonWithIconComponent } from '../../../shared/components/buttons/button-with-icon/button-with-icon.component';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [ButtonWithIconComponent],
+  imports: [FormsModule, ReactiveFormsModule, ButtonWithIconComponent],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.scss',
 })
-export class AddTaskComponent {}
+export class AddTaskComponent {
+  addTaskForm: FormGroup;
+
+  private fb = inject(FormBuilder);
+
+  constructor() {
+    this.addTaskForm = this.fb.group({
+      title: ['', Validators.required],
+      description: ['', Validators.required],
+      date: ['', Validators.required],
+    });
+  }
+
+  get title() {
+    return this.addTaskForm.get('title');
+  }
+
+  get description() {
+    return this.addTaskForm.get('description');
+  }
+
+  get date() {
+    return this.addTaskForm.get('date');
+  }
+
+  addTask() {
+    console.log(this.addTaskForm.value);
+  }
+
+  resetAddTaskForm() {
+    console.log('reset');
+  }
+}
