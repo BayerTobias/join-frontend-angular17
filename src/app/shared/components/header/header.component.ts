20492ffc31190bb893ae152dcebc103b98819e5f
BayerTobias <tbayer2@gmx.de>
2024-03-15 19:48:21 +0100

    header logout overlay

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -11,5 +11,16 @@ import { DataManagerService } from '../../../home/services/data-manager.service'
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  public logOutModal: boolean = false;
+
   public dataManager = inject(DataManagerService);
+
+  toggleLogOutModal() {
+    this.logOutModal = !this.logOutModal;
+  }
+
+  logout(event: Event) {
+    event.stopPropagation();
+    console.log('logout');
+  }
 }
