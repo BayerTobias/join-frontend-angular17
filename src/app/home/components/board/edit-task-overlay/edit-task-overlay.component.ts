fa7ad7affa070e7c1aa85f9b812f6f089b993b78
BayerTobias <tbayer2@gmx.de>
2024-03-17 18:33:19 +0100

    edit-task-overlay imports

--- src/app/home/components/board/edit-task-overlay/edit-task-overlay.component.ts
+++ src/app/home/components/board/edit-task-overlay/edit-task-overlay.component.ts
@@ -1,10 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { Task } from '../../../../classes/task.class';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ButtonWithIconComponent } from '../../../../shared/components/buttons/button-with-icon/button-with-icon.component';
 
 @Component({
   selector: 'app-edit-task-overlay',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
+    ButtonWithIconComponent,
+  ],
   templateUrl: './edit-task-overlay.component.html',
   styleUrl: './edit-task-overlay.component.scss',
 })
