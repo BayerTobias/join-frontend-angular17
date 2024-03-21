2dc871d2a82fd4777a949e93d2693981fe6cafae
BayerTobias <tbayer2@gmx.de>
2024-03-21 21:10:18 +0100

    edit task assigned-user

--- src/app/home/components/board/edit-task-overlay/edit-task-overlay.component.ts
+++ src/app/home/components/board/edit-task-overlay/edit-task-overlay.component.ts
@@ -7,11 +7,18 @@ import {
   inject,
 } from '@angular/core';
 import { Task } from '../../../../classes/task.class';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ButtonWithIconComponent } from '../../../../shared/components/buttons/button-with-icon/button-with-icon.component';
 import { UserSummary } from '../../../../classes/user-summary.class';
 import { DataManagerService } from '../../../services/data-manager.service';
+import { User } from '../../../../classes/user.class';
 
 @Component({
   selector: 'app-edit-task-overlay',
@@ -30,23 +37,75 @@ export class EditTaskOverlayComponent {
   @Output() closeOverlay: EventEmitter<void> = new EventEmitter<void>();
 
   public dataManager = inject(DataManagerService);
+  private fb = inject(FormBuilder);
 
+  editTaskForm: FormGroup;
   editTask: Task | null = null;
-
   users: UserSummary[] = [];
   userPickerOpen: boolean = false;
+  today: string = new Date().toISOString().split('T')[0];
+
+  prio: string = '';
 
   constructor() {
+    this.editTaskForm = this.fb.group({
+      title: ['', Validators.required],
+      description: ['', Validators.required],
+      date: ['', Validators.required],
+    });
+
     effect(() => {
       this.users = this.dataManager.usersSignal();
     });
   }
 
   ngOnInit() {
-    //erstelle eine kopie vom task
     this.editTask = JSON.parse(JSON.stringify(this.task));
+    this.updateForm();
+  }
 
-    console.log(this.editTask);
+  updateForm() {
+    this.editTaskForm.patchValue({
+      title: this.editTask?.title,
+      description: this.editTask?.description,
+      date: this.editTask?.dueDate,
+    });
+
+    if (this.editTask?.prio) this.prio = this.editTask?.prio;
+
+    console.log(this.prio);
+  }
+
+  setPrio(prio: string) {
+    this.prio = prio;
+  }
+
+  checkIfUserIsAssigned(user: UserSummary): boolean {
+    if (this.editTask && this.editTask.assignedToUserSummarys) {
+      return (
+        this.editTask.assignedToUserSummarys.find(
+          (assignedUser: UserSummary) => assignedUser.id === user.id
+        ) !== undefined
+      );
+    }
+    return false;
+  }
+
+  setAssignedUsers(userRef: UserSummary) {
+    userRef.checked = !userRef.checked;
+
+    if (this.editTask) {
+      this.editTask.assignedToUserSummarys = this.users.filter(
+        (user) => user.checked
+      );
+      this.editTask.assignedTo = [];
+    }
+
+    console.log(this.users);
+  }
+
+  saveEditedTask() {
+    console.log('Form Data', this.editTaskForm.value);
   }
 
   toggleUserPicker() {
