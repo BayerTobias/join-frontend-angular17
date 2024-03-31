9f0d4eec2e5bdb76beb650dd1610a1643d37cc44
BayerTobias <tbayer2@gmx.de>
2024-03-31 20:09:48 +0200

    reset password styling and form

--- src/app/auth/components/reset-password/reset-password.component.ts
+++ src/app/auth/components/reset-password/reset-password.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthBaseComponent } from '../auth-base/auth-base.component';
+import { ButtonWoIconComponent } from '../../../shared/components/buttons/button-wo-icon/button-wo-icon.component';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-reset-password',
   standalone: true,
-  imports: [],
+  imports: [
+    AuthBaseComponent,
+    ButtonWoIconComponent,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+  ],
   templateUrl: './reset-password.component.html',
-  styleUrl: './reset-password.component.scss'
+  styleUrl: './reset-password.component.scss',
 })
 export class ResetPasswordComponent {
+  setPasswordForm: FormGroup;
+  passwordIsHidden: boolean = true;
+  passwordRepeatIsHidden: boolean = true;
 
+  private fb = inject(FormBuilder);
+
+  constructor() {
+    this.setPasswordForm = this.fb.group({
+      newPassword: ['', Validators.required],
+      passwordRepeat: ['', Validators.required],
+    });
+  }
+
+  get newPassword() {
+    return this.setPasswordForm.get('newPassword');
+  }
+
+  get passwordRepeat() {
+    return this.setPasswordForm.get('passwordRepeat');
+  }
+
+  togglePasswordVisibility() {
+    this.passwordIsHidden = !this.passwordIsHidden;
+  }
+
+  togglePasswordRepeatVisibility() {
+    this.passwordRepeatIsHidden = !this.passwordRepeatIsHidden;
+  }
+
+  setNewPassword() {
+    console.log(this.setPasswordForm);
+  }
 }
--- src/app/legal/components/legal-notice/legal-notice.component.ts
+++ src/app/legal/components/legal-notice/legal-notice.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-legal-notice',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss',
 })
 export class LegalNoticeComponent {
+  private router = inject(Router);
+
+  isNotHomeRoute() {
+    return this.router.url === '/legal-notice';
+  }
+
   previousPage() {
     window.history.back();
   }
